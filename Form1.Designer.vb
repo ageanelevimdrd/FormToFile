f712ed6de77e7e45179db296f61875bf898060db
ageanelevimdrd <madridageanelevi@gmail.com>
2020-02-15 03:15:31 +0800

    Data saving into .txt file

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,128 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.NameBox = New System.Windows.Forms.TextBox()
+        Me.AgeBox = New System.Windows.Forms.TextBox()
+        Me.AddressBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'NameBox
+        '
+        Me.NameBox.Location = New System.Drawing.Point(77, 30)
+        Me.NameBox.Multiline = True
+        Me.NameBox.Name = "NameBox"
+        Me.NameBox.Size = New System.Drawing.Size(555, 25)
+        Me.NameBox.TabIndex = 1
+        '
+        'AgeBox
+        '
+        Me.AgeBox.Location = New System.Drawing.Point(77, 73)
+        Me.AgeBox.Multiline = True
+        Me.AgeBox.Name = "AgeBox"
+        Me.AgeBox.Size = New System.Drawing.Size(206, 25)
+        Me.AgeBox.TabIndex = 3
+        '
+        'AddressBox
+        '
+        Me.AddressBox.Location = New System.Drawing.Point(97, 112)
+        Me.AddressBox.Multiline = True
+        Me.AddressBox.Name = "AddressBox"
+        Me.AddressBox.Size = New System.Drawing.Size(584, 25)
+        Me.AddressBox.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(157, 184)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Save as txt file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(297, 184)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save as XML file"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(439, 184)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(110, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Save as JSON file"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 20)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Name: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Age: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 20)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Address: "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(691, 228)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.AddressBox)
+        Me.Controls.Add(Me.AgeBox)
+        Me.Controls.Add(Me.NameBox)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents NameBox As TextBox
+    Friend WithEvents AgeBox As TextBox
+    Friend WithEvents AddressBox As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,27 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Public Class Form1
+    Dim sw1 As StreamWriter
 
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If NameBox.Text <> "" Or AgeBox.Text <> "" Or AddressBox.Text <> "" Then
+            sw1 = File.AppendText("C:\Users\user\Documents\COLLEGE\Acads (5th yr)\electives3\FormToFile\FormToFile\FormToFile.txt")
+            sw1.WriteLine(Label1.Text + NameBox.Text + vbNewLine + Label2.Text + AgeBox.Text + vbNewLine + Label3.Text + AddressBox.Text)
+            MsgBox("Data has been saved to a txt file", MsgBoxStyle.Information, "Saved")
+            sw1.Close()
+        Else
+            MsgBox("Please input a data into boxes", MsgBoxStyle.Critical, "Input data")
+        End If
+    End Sub
+
+    Private Sub NameBox_TextChanged(sender As Object, e As EventArgs) Handles NameBox.TextChanged
+
+    End Sub
 End Class
