43b5c550445f33bf3d639085fe8655000a3dbfea
ageanelevimdrd <madridageanelevi@gmail.com>
2020-02-15 03:42:16 +0800

    Data saving into .xml file

--- Form1.vb
+++ Form1.vb
@@ -10,18 +10,46 @@ Public Class Form1
 
     End Sub
 
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If NameBox.Text <> "" Or AgeBox.Text <> "" Or AddressBox.Text <> "" Then
-            sw1 = File.AppendText("C:\Users\user\Documents\COLLEGE\Acads (5th yr)\electives3\FormToFile\FormToFile\FormToFile.txt")
+            sw1 = File.AppendText("C:\Users\user\Documents\COLLEGE\Acads (5th yr)\electives3\FormToFile\FormToFile.txt")
             sw1.WriteLine(Label1.Text + NameBox.Text + vbNewLine + Label2.Text + AgeBox.Text + vbNewLine + Label3.Text + AddressBox.Text)
-            MsgBox("Data has been saved to a txt file", MsgBoxStyle.Information, "Saved")
+            MsgBox("Saved successfully!", MsgBoxStyle.Information, "Saved")
+            NameBox.Clear()
+            AgeBox.Clear()
+            AddressBox.Clear()
             sw1.Close()
         Else
             MsgBox("Please input a data into boxes", MsgBoxStyle.Critical, "Input data")
         End If
     End Sub
 
-    Private Sub NameBox_TextChanged(sender As Object, e As EventArgs) Handles NameBox.TextChanged
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If NameBox.Text <> "" Or AgeBox.Text <> "" Or AddressBox.Text <> "" Then
+            sw1 = File.AppendText("C:\Users\user\Documents\COLLEGE\Acads (5th yr)\electives3\FormToFile\FormToFile.xml")
+            sw1.WriteLine(Label1.Text + NameBox.Text + vbNewLine + Label2.Text + AgeBox.Text + vbNewLine + Label3.Text + AddressBox.Text)
+            MsgBox("Saved successfully!", MsgBoxStyle.Information, "Saved")
+            NameBox.Clear()
+            AgeBox.Clear()
+            AddressBox.Clear()
+            sw1.Close()
+        Else
+            MsgBox("Please input a data into boxes", MsgBoxStyle.Critical, "Input data")
+        End If
+    End Sub
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If NameBox.Text <> "" Or AgeBox.Text <> "" Or AddressBox.Text <> "" Then
+            sw1 = File.AppendText("C:\Users\user\Documents\COLLEGE\Acads (5th yr)\electives3\FormToFile\FormToFile.json")
+            sw1.WriteLine(Label1.Text + NameBox.Text + vbNewLine + Label2.Text + AgeBox.Text + vbNewLine + Label3.Text + AddressBox.Text)
+            MsgBox("Saved successfully!", MsgBoxStyle.Information, "Saved")
+            NameBox.Clear()
+            AgeBox.Clear()
+            AddressBox.Clear()
+            sw1.Close()
+        Else
+            MsgBox("Please input a data into boxes", MsgBoxStyle.Critical, "Input data")
+        End If
     End Sub
 End Class
